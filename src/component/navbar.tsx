2581efa730c28472ef21af9d62962e84e75873b3
ezedike evan <egwomevan323@gmail.com>
2025-04-27 12:48:01 +0100

    ADD: dashboard navigation bar

--- src/component/navbar.tsx
+++ src/component/navbar.tsx
@@ -1,11 +1,45 @@
+import { act, useState } from 'react'
+import { FaSearch } from 'react-icons/fa'
+
 const Navbar = () => {
+	const [activeTab, setActiveTab] = useState<'token' | 'whales'>('token')
 	return (
-		<nav>
-			<div>
-				<input type="text" />
+		<section className="flex flex-col mt-10 gap-5">
+			<div className="flex border items-center rounded-xl px-4 gap-3">
+				<FaSearch size={24} />
+				<input
+					type="text"
+					className="p-3 w-full outline-none"
+					placeholder={
+						activeTab === 'token'
+							? 'Explore Our Archives'
+							: 'Trace the tides of whales'
+					}
+				/>
 			</div>
-			this is the nav bar
-		</nav>
+			<nav className="bg-[#80808080] flex justify-evenly mx-25 py-2 rounded-xl font-semibold">
+				<p
+					className={`${
+						activeTab === 'token' && 'bg-[#161615]'
+					} px-20 py-1 rounded-md cursor-pointer`}
+					onClick={() => {
+						setActiveTab('token')
+					}}
+				>
+					Token Archive
+				</p>
+				<p
+					className={`${
+						activeTab === 'whales' && 'bg-[#161615]'
+					} px-20 py-1 rounded-md cursor-pointer`}
+					onClick={() => {
+						setActiveTab('whales')
+					}}
+				>
+					Vault Of Whales
+				</p>
+			</nav>
+		</section>
 	)
 }
 
